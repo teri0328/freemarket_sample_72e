d06a3a7414d7dd496c30daab322d1969e38778a1
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-04-01 10:42:28 +0900

    create "Product"model

---
+++ db/migrate/20200401013051_create_products.rb
@@ -0,0 +1,18 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.text       :explanation  , null: false
+      t.string     :name         , null: false
+      t.string     :region       , null: false
+      t.string     :size
+      t.integer    :price        , null: false
+      t.integer    :shipping_days, null: false
+      t.boolean    :postage      , null: false, default: false
+      t.references :user         , null: false, foreign_key: true
+      t.references :condition    , null: false, foreign_key: true
+      t.references :category     , null: false, foreign_key: true
+      t.references :bland        , null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
