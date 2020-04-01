36fa8dca0384230dac7332b5d6bc800daef5e275
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-04-01 12:36:10 +0900

    create "Image"model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200401033156) do
+ActiveRecord::Schema.define(version: 20200401033427) do
 
   create_table "blands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 20200401033156) do
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_evaluations_on_product_id", using: :btree
     t.index ["user_id"], name: "index_evaluations_on_user_id", using: :btree
+  end
+
+  create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "image",      null: false
+    t.integer  "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_images_on_product_id", using: :btree
   end
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
