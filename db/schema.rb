c76b8528eaac363679bc98044e46bddb5b7b871b
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-04-01 11:46:32 +0900

    create "Bland"model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200401024311) do
+ActiveRecord::Schema.define(version: 20200401024505) do
+
+  create_table "blands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
