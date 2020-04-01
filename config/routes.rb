035f253162ac645aaaa694baec2b3f72752355cd
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-04-01 15:26:15 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root "products#index"
-  resources :products, only: [:index,:new]
+  resources :products, only: [:index,:new] do
+    collection do
+      get "buy"
+    end
+  end
+  resources :users, only: [:index] do
+    collection do
+      get "about"
+    end
+  end
 end
