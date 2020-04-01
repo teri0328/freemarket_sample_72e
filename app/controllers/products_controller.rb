351339e524edc72dbf2fd40d5b74f22cc04c7884
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-04-01 15:01:55 +0900

    create "Products#new"view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,17 @@
 class ProductsController < ApplicationController
+  before_action :set_params
+
   def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def set_params
   end
 end
