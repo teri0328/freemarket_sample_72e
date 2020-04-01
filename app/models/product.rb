5ba00f781f18b199c46b3c826b34fd3bb630f7d6
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-04-01 17:03:51 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   belongs_to :bland
   belongs_to :evaluation
   has_many :conditions
-  has_many :categories, through: product_category, dependent: :destroy
+  has_many :categories, through: :product_categories, dependent: :destroy
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
 end
