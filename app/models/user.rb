3f7ccf012a97044c8aecceeff91f07fd137b7182
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-04-04 16:59:06 +0900

    新規登録機能実装まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :products, dependent: :destroy
-  has_many :cards, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  has_many :evaluations, dependent: :destroy
+  has_many  :products           , dependent: :destroy
+  has_many  :cards              , dependent: :destroy
+  has_many  :likes              , dependent: :destroy
+  has_many  :evaluations        , dependent: :destroy
+  # validates :encrypted_password , length: {minimum: 7}
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
