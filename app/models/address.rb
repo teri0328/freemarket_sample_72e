b1ecf83eb81d5334639a76f1fed7a2c7cdefd191
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-04-05 23:05:48 +0900

    ビューの調整

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
   validates :post_number, :prefecture, :city, :address, presence: true
-  
-  validates :post_number, format: {with: /\A[ぁ-んァ-ン一-龥]/}
 end
