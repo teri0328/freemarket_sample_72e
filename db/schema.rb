6a00ce4b74542fa171fa4f189da7bb7103578a0f
LieuFukunaga <heartarium01208.2@gmail.com>
2020-04-06 15:00:00 +0900

    rails db:migrate for categories table to add ancestry column

--- db/schema.rb
+++ db/schema.rb
@@ -136,6 +136,7 @@ ActiveRecord::Schema.define(version: 20200417102557) do
     t.integer  "birth_year",                          null: false
     t.integer  "birth_month",                         null: false
     t.integer  "birth_day",                           null: false
+    t.string   "tel_number",                          null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -150,7 +151,6 @@ ActiveRecord::Schema.define(version: 20200417102557) do
     t.string   "lastname_kana",                       null: false
     t.string   "email"
     t.string   "nickname",                            null: false
-    t.string   "tel_number"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
