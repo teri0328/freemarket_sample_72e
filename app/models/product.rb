0f4f13af718a3162021ee43c2e446a396e400256
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-04-10 09:04:33 +0900

    アソシエーション修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :bland
   belongs_to :condition
-  has_one :category
+  belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
 end
