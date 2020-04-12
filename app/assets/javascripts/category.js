268a21675f3afa068749ca34971c187065d34c78
LieuFukunaga <heartarium01208.2@gmail.com>
2020-04-13 08:56:41 +0900

    実装：表示切り替え（第一・第二階層）

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,44 +1,32 @@
 $(function() {
+  // カテゴリー表示領域にマウスポインターが乗った時、すべての第一階層を表示する
   $(".category-area").on("mouseenter", function(){
+    $(".gen1s").removeClass("negative");
     $("p.gen1").removeClass("negative");
   });
-  $(".category-container").on("mouseleave", function(){
-    $("p.gen1").addClass("negative");
-  });
+
+  // 第一階層にマウスポインターが乗ったときに対応する第二階層だけを表示にする
   $("p.gen1").on("mouseenter", function(){
     let gen1_class = $(this).attr("class");
     gen1_class = gen1_class.replace("gen1", "");
     gen1_class = gen1_class.replace(" gen1", "gen1");
+    $(".gen2s").removeClass("negative");
+    $("p.gen2").addClass("negative").not(`.${gen1_class}_children`);
     $(`.${gen1_class}_children`).removeClass("negative");
-    console.log(`${gen1_class}`);
   });
-  $("p.gen1").on("mouseleave", function(){
-    let gen1_class = $(this).attr("class");
-    gen1_class = gen1_class.replace("gen1", "");
-    gen1_class = gen1_class.replace(" gen1", "gen1");
-    $(`.${gen1_class}_children`).addClass("negative");
-  });
-  // $(".category-container").on("mouseleave", function(){
-  //   $("p.gen1").addClass("negative");
-  //   $("p.gen2").addClass("negative");
-  //   $("p.gen3").addClass("negative");
+
+  // 第二階層にマウスポインターが乗ったときに対応する第三階層だけを表示させる
+  // $("p.gen2").on("mouseenter", function(){
+  //   let gen2_class = $(this).attr("class");
+  //   gen2_class = gen2_class.replace("gen2", "");
+  //   gen2_class = gen2_class.replace("negative", "");
+  //   $(`.${gen2_class}`).removeClass("negative");
   // });
+
+  // カテゴリー表示領域からマウスポインターが離れた時、すべての階層を非表示にする
+  $(".category-area").on("mouseleave", function(){
+    $("[class^='gen1']").addClass("negative");
+    $("[class^='gen2']").addClass("negative");
+    $("[class^='gen3']").addClass("negative");
+  });
 });
-
-// $(function() {
-//   $("p.categories").on("mouseover", function(){
-//     $(".negative").removeClass("negative");
-//   });
-//   $(".gen1s").on("mouseout", function(){
-//     $(".gen1s").addClass("negative");
-//   });
-// });
-
-// $(function() {
-//   $(".category").on("mouseover", function(){
-//     $(".category").append('<li class = "append_list">appendメソッドで追加した要素です"</li>');
-//   });
-//   $(".category").on("mouseout", function(){
-//     $(".append_list").remove();
-//   });
-// });
