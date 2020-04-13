2a688d1da2bbfcc64b57284eff3e95e9356a8c97
LieuFukunaga <heartarium01208.2@gmail.com>
2020-04-13 11:40:14 +0900

    update: 多重カテゴリの表示切り替え（要改善点あり）

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,27 +1,41 @@
 $(function() {
-  // カテゴリー表示領域にマウスポインターが乗った時、すべての第一階層を表示する
+  // カテゴリー表示領域にマウスポインターが乗った時の処理
   $(".category-area").on("mouseenter", function(){
     $(".gen1s").removeClass("negative");
     $("p.gen1").removeClass("negative");
   });
 
-  // 第一階層にマウスポインターが乗ったときに対応する第二階層だけを表示にする
+  // 第一階層にマウスポインターが乗ったときの処理
   $("p.gen1").on("mouseenter", function(){
-    let gen1_class = $(this).attr("class");
-    gen1_class = gen1_class.replace("gen1", "");
-    gen1_class = gen1_class.replace(" gen1", "gen1");
+    $(this).css('background-color', 'red');
+    let current_gen1_class = $(this).attr("class");
+    current_gen1_class = current_gen1_class.replace("gen1", "");
+    current_gen1_class = current_gen1_class.replace(" gen1", "gen1");
     $(".gen2s").removeClass("negative");
-    $("p.gen2").addClass("negative").not(`.${gen1_class}_children`);
-    $(`.${gen1_class}_children`).removeClass("negative");
+    $("p.gen2").addClass("negative").not(`.${current_gen1_class}_children`);
+    $(`.${current_gen1_class}_children`).removeClass("negative");
+  });
+  // 第一階層からマウスポインターが離れたときの処理
+  $("p.gen1").on("mouseleave", function(){
+    let leaved_gen1_class = $(this).attr("class");
+    leaved_gen1_class = leaved_gen1_class.replace("gen1", "");
+    leaved_gen1_class = leaved_gen1_class.replace(" gen1", "gen1");
+    $(`.${leaved_gen1_class}`).css('background-color', '');
   });
 
-  // 第二階層にマウスポインターが乗ったときに対応する第三階層だけを表示させる
-  // $("p.gen2").on("mouseenter", function(){
-  //   let gen2_class = $(this).attr("class");
-  //   gen2_class = gen2_class.replace("gen2", "");
-  //   gen2_class = gen2_class.replace("negative", "");
-  //   $(`.${gen2_class}`).removeClass("negative");
-  // });
+  // 第二階層にマウスポインターが乗ったときの処理
+  $("p.gen2").on("mouseenter", function(){
+    let gen2_class = $(this).attr("class");
+    gen2_class = gen2_class.replace("gen2", "");
+    gen2_class = gen2_class.replace("negative", "");
+
+    // マウスポインターが乗ったgen2のクラス名を元にgen1のクラス名を取得
+    parent_class = gen2_class.replace("_children", "");
+    console.log(`${parent_class}`);
+    $(".gen1").css('background-color', '').not(`.${parent_class}`);
+    $(`.${parent_class}`).css('background-color', 'red');
+
+  });
 
   // カテゴリー表示領域からマウスポインターが離れた時、すべての階層を非表示にする
   $(".category-area").on("mouseleave", function(){
