f3b56d9e4e6509dfb17a2bdec02a89bb19aa0e81
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-04-14 23:00:49 +0900

    test

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -12,17 +12,16 @@ class CardController < ApplicationController
 
   def create
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-    # if params['payjpToken'].blank?
-    #   redirect_to action: "new"
-    # else
+    if params['payjpToken'].blank?
+      redirect_to action: "new"
+    else
       customer = Payjp::Customer.create(card: params['payjpToken'])
       card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      card.save!
-      # if card.save!
-      #   redirect_to card_path(card)
-      # else
-      #   redirect_to action: "new"
-      # end
+      if card.save!
+        redirect_to card_path(card)
+      else
+        redirect_to action: "new"
+      end
     end
   end
 
