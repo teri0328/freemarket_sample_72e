a1d49ba3a77a255ae24d3a07f39986529c40ea3e
LieuFukunaga <heartarium01208.2@gmail.com>
2020-04-18 10:29:46 +0900

    Productモデルの不要な記述を削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,4 @@ class Product < ApplicationRecord
   has_many :images  , dependent: :destroy
   has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :images
-
-  gen1_json = Category.where(ancestry: nil)
-  gen1_json.to_json
 end
