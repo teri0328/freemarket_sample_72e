e4a758f8de09a352aaf82d57c1c92dc34b1bb5c0
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-04-18 13:42:29 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,9 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.12.1'
 
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{ config/secrets.yml }
+
 # Capistranoのログの表示に利用する
 set :application, 'freemarket_sample_72e'
 
@@ -31,6 +34,18 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
 
 # set :default_env, {
